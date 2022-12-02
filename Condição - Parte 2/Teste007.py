b763ff411d68767d4e10c446ca4d20d3bee3a57f
QuietKadu <kadukauamorais@outlook.com>
2022-12-02 20:42:42 -0300

    Teste de Condição 2

--- Condição - Parte 2/Teste007.py
+++ Condição - Parte 2/Teste007.py
@@ -1,6 +1,6 @@
 pe = float(input('Qual é seu peso?: '))
 al = float(input('Qual é sua altura?: '))
-imc = pe / (al ** al)
+imc = pe / (al ** 2)
 if imc < 18.5:
     print('Abaixo do Peso')
 elif 18.5 <= imc < 25:
