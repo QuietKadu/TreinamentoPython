9e7b9a12d7503312f77b5082ec1c055dc24e864c
QuietKadu <kadukauamorais@outlook.com>
2023-01-23 22:17:56 -0300

    Recolocando Arquivos

--- Listas/Teste001.py
+++
@@ -1,6 +0,0 @@
-lista = []
-for li in range(0, 6):
-    lista.append(int(input(f'Digite um valor para posição {li}: ')))
-print(f'Você digitou os valores {lista}')
-print(f'o maior valor foi {max(lista)} e foi encontrado na posição {lista.index(max(lista))}')
-print(f'O menor valor foi {min(lista)} e foi encontrado na posição {lista.index(min(lista))}')
