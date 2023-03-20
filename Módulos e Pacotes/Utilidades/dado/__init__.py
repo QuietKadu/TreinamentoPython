0f3a4d133374161a481f2876ba320cee91570620
QuietKadu <kadukauamorais@outlook.com>
2023-03-20 12:47:07 -0300

    Teste de Módulos e Pacotes

--- Módulos e Pacotes/Utilidades/dado/__init__.py
+++
@@ -1,9 +0,0 @@
-def leiadinheiro(msg):
-    valido = False
-    while not valido:
-        entrada = str(input(msg)).replace(',', '.').strip()
-        if entrada.isalpha() or entrada == '':
-            print(f'Erro: \"{entrada}\" é um preço inválido')
-        else:
-            valido = True
-            return float(entrada)
