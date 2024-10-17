7471728a6f7f277f43d305d49cd4139473f5bd59
Kadu Kauã <118638730+QuietKadu@users.noreply.github.com>
2024-10-17 18:56:06 -0300

    Automatizando excel com python

--- Tecnico_Python/Exercicio_Especial/Exercicio_Especial2.py
+++ Tecnico_Python/Exercicio_Especial/Exercicio_Especial2.py
@@ -1,6 +1,67 @@
 from tkinter import *
+import openpyxl
 
 def main_program():
+    window = Tk()
+    window.geometry("1920x1080")
+    window.configure(background="#FFCC66")
+    window.title("Grafico")
+
+    answers1 = StringVar()
+    answers2 = StringVar()
+    answers3 = StringVar()
+
+    question1 = Label(window, text="1 - Quanto tempo você passa utilizando dispositivos eletronicos (Computador, celular e tablet) por dia?", background="#FFCC66", foreground="#000", font=("Helvetica", 13))
+    question1.pack()
+
+    Alternatives1 = Radiobutton(window, text="a) 2 - 3 horas", background="#FFCC66", value="a", variable=answers1, font=("Helvetica", 13))
+    Alternatives1.pack()
+
+    Alternatives2 = Radiobutton(window, text="Mais de 3 horas", background="#FFCC66", value="b", variable=answers1, font=("Helvetica", 13))
+    Alternatives2.pack()
+
+    Alternatives3 = Radiobutton(window, text="c) Não utiliza", background="#FFCC66", value="c", variable=answers1, font=("Helvetica", 13))
+    Alternatives3.pack()
+
+    question2 = Label(window, text="2 - Quanto você fica nas redes sociais?", background="#FFCC66", foreground="#000", font=("Helvetica", 13))
+    question2.pack()
+
+    Alternatives4 = Radiobutton(window, text="a) 1 - 2 horas", background="#FFCC66", value="d", variable=answers2, font=("Helvetica", 13))
+    Alternatives4.pack()
+
+    Alternatives5 = Radiobutton(window, text="b) Mais de 3 horas", background="#FFCC66", value="e", variable=answers2, font=("Helvetica", 13))
+    Alternatives5.pack()
+
+    Alternatives6 = Radiobutton(window, text="c) Não utiliza", background="#FFCC66", value="f", variable=answers2, font=("Helvetica", 13))
+    Alternatives6.pack()
+
+    question3 = Label(window, text="3 - Quanto tempo você faz pausa?", background="#FFCC66", foreground="#000", font=("Helvetica", 13))
+    question3.pack()
+
+    Alternatives7 = Radiobutton(window, text="a) 30 - 60 minutos", background="#FFCC66", value="g", variable=answers3, font=("Helvetica", 13))
+    Alternatives7.pack()
+
+    Alternatives8 = Radiobutton(window, text="b) Raramente faz pausa", background="#FFCC66", value="", variable=answers3, font=("Helvetica", 13))
+    Alternatives8.pack()
+
+    Alternatives9 = Radiobutton(window, text="Não se aplica", background="#FFCC66", value="i", variable=answers3,font=("Helvetica", 13))
+    Alternatives9.pack()
+
+    Button(window, text="Enviar resposta").place(x=912, y=360, width=100, height=20)
+
+
+
+    window.mainloop()
+
+
+def exel():
+    book = openpyxl.workbook()
+    book.create_sheet("Grafico")
+    graphic_page = book["Grafico"]
+
+
+
+def third_program():
     choise = "s"
     quant = 0
     saud = 0
@@ -55,54 +116,3 @@ def main_program():
             nulo = nulo + 1
             print("Você utiliza de forna netra")
         choise = input("Deseja continuar? [S/N]: ")
-
-
-window = Tk()
-window.geometry("1920x1080")
-window.configure(background="#FFCC66")
-window.title("Grafico")
-
-answers1 = StringVar()
-answers2 = StringVar()
-answers3 = StringVar()
-
-question1 = Label(window, text="1 - Quanto tempo você passa utilizando dispositivos eletronicos (Computador, celular e tablet) por dia?", background="#FFCC66", foreground="#000", font=("Helvetica", 13))
-question1.pack()
-
-Alternatives1 = Radiobutton(window, text="a) 2 - 3 horas", background="#FFCC66", value="a", variable=answers1, font=("Helvetica", 13))
-Alternatives1.pack()
-
-Alternatives2 = Radiobutton(window, text="Mais de 3 horas", background="#FFCC66", value="b", variable=answers1, font=("Helvetica", 13))
-Alternatives2.pack()
-
-Alternatives3 = Radiobutton(window, text="c) Não utiliza", background="#FFCC66", value="c", variable=answers1, font=("Helvetica", 13))
-Alternatives3.pack()
-
-question2 = Label(window, text="2 - Quanto você fica nas redes sociais?", background="#FFCC66", foreground="#000", font=("Helvetica", 13))
-question2.pack()
-
-Alternatives4 = Radiobutton(window, text="a) 1 - 2 horas", background="#FFCC66", value="d", variable=answers2, font=("Helvetica", 13))
-Alternatives4.pack()
-
-Alternatives5 = Radiobutton(window, text="b) Mais de 3 horas", background="#FFCC66", value="e", variable=answers2, font=("Helvetica", 13))
-Alternatives5.pack()
-
-Alternatives6 = Radiobutton(window, text="c) Não utiliza", background="#FFCC66", value="f", variable=answers2, font=("Helvetica", 13))
-Alternatives6.pack()
-
-question3 = Label(window, text="3 - Quanto tempo você faz pausa?", background="#FFCC66", foreground="#000", font=("Helvetica", 13))
-question3.pack()
-
-Alternatives7 = Radiobutton(window, text="a) 30 - 60 minutos", background="#FFCC66", value="g", variable=answers3, font=("Helvetica", 13))
-Alternatives7.pack()
-
-Alternatives8 = Radiobutton(window, text="b) Raramente faz pausa", background="#FFCC66", value="", variable=answers3, font=("Helvetica", 13))
-Alternatives8.pack()
-
-Alternatives9 = Radiobutton(window, text="Não se aplica", background="#FFCC66", value="i", variable=answers3,font=("Helvetica", 13))
-Alternatives9.pack()
-
-
-
-
-window.mainloop()
